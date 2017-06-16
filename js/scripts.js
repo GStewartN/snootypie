f5955ea8b4edfccc506095e955389b4e7e1e2103
GStewartN <stewart.nathang@gmail.com>
2017-06-16 13:24:30 -0700

    change topping id, add logic for cost calculation

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
+
 function Pizza() {
   this.size;
-  this.meat = [];
-  this.toppings = [];
+  this.meat;
+  this.topping;
+  this.cost;
 }
 
-
-
-
-
-
-
-
+Pizza.prototype.totalCost = function () {
+  this.cost = this.size += this.meat += this.topping;
+  return this.cost;
+}
 
 
 $(document).ready(function() {
 
+  $("form#pizza").submit(function(event) {
+    event.preventDefault();
+
+    var newPizza = new Pizza();
+
+    var size = $("#size").val();
+    var meat = $("#meat").val();
+    var topping = $("#topping").val();
+    var cost = newPizza.totalCost();
+
+    $("#price").text(cost);
+
+  });
 });
