4919e93f4564f85d1c10b4601fe05ca198b3b0bf
GStewartN <stewart.nathang@gmail.com>
2017-06-16 15:44:20 -0700

    change form id, rework logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-
-function Pizza() {
-  this.crustSize;
-  this.meat;
-  this.topping;
-  this.cost;
+function Pizza(crustSize, meat, topping) {
+  this.crustSize = crustSize;
+  this.meat = meat;
+  this.topping = topping;
+  this.cost = 0;
 }
 
 Pizza.prototype.totalCost = function() {
@@ -17,9 +16,9 @@ $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
 
-    var newPizza = new Pizza();
+    var newPizza = new Pizza(crustSize, meat, topping);
 
-    var crustSize = parseInt($("#size").val());
+    var crustSize = parseInt($("#crust-size").val());
     var meat = parseInt($("#meat").val());
     var topping = parseInt($("#topping").val());
     var cost = newPizza.totalCost();
